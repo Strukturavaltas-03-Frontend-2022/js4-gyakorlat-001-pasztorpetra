cb49878faa29271ca7e0619324b8e0967054ff32
Petra <pasztor.petra1998@gmail.com>
2022-09-10 19:57:56 +0200

    1.feladat kész

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -4,6 +4,20 @@
 // ami az eredeti tömb összes elemét nagybetűssé alakítva tartalmazza. Ellenkező esetben a Promise 
 // rejected lesz, a visszatérési érték a következő string: 'Error: Not all items in the array are strings!'
 
+'use strict'
 
+const convertToUppercase = (arr = []) => {
+  return new Promise ((resolve, reject) => {
+    if (arr.every(item => (typeof item === 'string'))) {
+      resolve(arr.map ((item) => item.toUpperCase()))
+    } else {
+      reject ('Error: Not all items in the array are strings!')
+    }
+  })
+}
+
+convertToUppercase()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error))
 
 export default convertToUppercase;
