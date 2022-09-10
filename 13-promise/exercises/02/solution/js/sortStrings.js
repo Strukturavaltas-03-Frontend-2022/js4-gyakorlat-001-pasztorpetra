2cfb374bfbd590f397a7f107b9456030babf1510
Petra <pasztor.petra1998@gmail.com>
2022-09-10 19:22:21 +0200

    start

--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,8 @@
+// 2. Írj egy másik függvényt `sortStrings()` néven, mely paraméterként egy tömböt kap. 
+// A függvény visszatérési érték egy Promise. A Promise resolved lesz, amennyiben a tömb minden eleme 
+// string, a resolved value az ABC szerinti rendezett tömb. Ha nem minden elem string, 
+// akkor a Promise rejected lesz, az Error objektum üzenet szövege:'Error: Not all items in the array are strings!'   
+// Ha a sorba rendezés során hiba történik akkor is rejected lesz a Promise, az Error objektum 
+// üzenet szövege ebben az esetben: 'Error: Something went wrong with sorting words.'
+
 export default sortStrings;
